dac35da509f211598d5fa00f9e403677c431169b
Joshua Carp <jm.carp@gmail.com>
2016-03-13 18:42:54 -0400

    Add tests.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import find_packages
 
 REQUIRES = [
     'six',
+    'psycopg2',
     'sqlalchemy',
 ]
 
---
+++ tests/test_export.py
@@ -0,0 +1,63 @@
+import io
+
+import pytest
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy_export import export
+
+Base = declarative_base()
+engine = sa.create_engine('postgresql:///export-test')
+
+class Album(Base):
+    __tablename__ = 'album'
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.Text)
+
+@pytest.fixture()
+def session():
+    Session = sa.orm.sessionmaker(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    return Session()
+
+@pytest.yield_fixture
+def objects(session):
+    rows = [
+        Album(name='A Night at the Opera'),
+        Album(name='A Day at the Races'),
+        Album(name='Jazz'),
+    ]
+    for row in rows:
+        session.add(row)
+    session.commit()
+    try:
+        yield rows
+    finally:
+        for row in rows:
+            session.delete(row)
+        session.commit()
+
+class TestExport:
+
+    def test_export_query(self, session, objects):
+        sio = io.StringIO()
+        export(session.query(Album), session.connection().engine, sio)
+        lines = sio.getvalue().strip().split('\n')
+        assert len(lines) == 3
+        assert lines[0].split('\t') == [str(objects[0].id), objects[0].name]
+
+    def test_export_table(self, session, objects):
+        sio = io.StringIO()
+        export(Album.__table__.select(), session.connection().engine, sio)
+        lines = sio.getvalue().strip().split('\n')
+        assert len(lines) == 3
+        assert lines[0].split('\t') == [str(objects[0].id), objects[0].name]
+
+    def test_export_csv(self, session, objects):
+        sio = io.StringIO()
+        flags = {'format': 'csv', 'header': True}
+        export(session.query(Album), session.connection().engine, sio, **flags)
+        lines = sio.getvalue().strip().split('\n')
+        assert len(lines) == 4
+        assert lines[0].split(',') == ['id', 'name']
+        assert lines[1].split(',') == [str(objects[0].id), objects[0].name]
