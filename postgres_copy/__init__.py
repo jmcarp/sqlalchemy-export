d4992a018f0525788aa7074e908cdf99587a3f10
Joshua Carp <jm.carp@gmail.com>
2016-03-19 15:33:44 -0400

    Add copy from wrapper.

--- postgres_copy/__init__.py
+++ postgres_copy/__init__.py
@@ -1,38 +1,62 @@
 import six
 
 from sqlalchemy.sql import ColumnElement
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Mapper, class_mapper
-from sqlalchemy.orm.exc import UnmappedClassError
 from sqlalchemy.dialects import postgresql
 
 __version__ = '0.1.0'
 
-def copy_to(select, engine, fp, **flags):
+def copy_to(source, dest, engine, **flags):
     """Export a query or select to a file. For flags, see the PostgreSQL
     documentation at http://www.postgresql.org/docs/9.5/static/sql-copy.html.
 
     Examples: ::
         select = MyTable.select()
         with open('/path/to/file.tsv', 'w') as fp:
-            export(select, engine, fp)
+            copy_to(select, fp, engine)
 
         query = session.query(MyModel)
         with open('/path/to/file/csv', 'w') as fp:
-            export(query, engine, fp, format='csv', null='.')
+            copy_to(query, fp, engine, format='csv', null='.')
 
-    :param select: SQLAlchemy query or select
+    :param source: SQLAlchemy query or select
+    :param dest: Destination file pointer, in write mode
     :param engine: SQLAlchemy engine
-    :param fp: File pointer, using write mode
     """
     dialect = postgresql.dialect()
-    statement = getattr(select, 'statement', select)
+    statement = getattr(source, 'statement', source)
     compiled = statement.compile(dialect=dialect)
     conn = engine.raw_connection()
     cursor = conn.cursor()
     query = cursor.mogrify(compiled.string, compiled.params).decode()
     formatted_flags = '({})'.format(format_flags(flags)) if flags else ''
     copy = 'COPY ({}) TO STDOUT {}'.format(query, formatted_flags)
-    cursor.copy_expert(copy, fp)
+    cursor.copy_expert(copy, dest)
+    conn.close()
+
+def copy_from(source, dest, engine, **flags):
+    """Import a table from a file. For flags, see the PostgreSQL documentation
+    at http://www.postgresql.org/docs/9.5/static/sql-copy.html.
+
+    Examples: ::
+        with open('/path/to/file.tsv') as fp:
+            copy_from(fp, MyTable, engine)
+
+        with open('/path/to/file.csv') as fp:
+            copy_from(fp, MyModel, engine, format='csv')
+
+    :param source: Source file pointer, in read mode
+    :param dest: SQLAlchemy model or table
+    :param engine: SQLAlchemy engine
+    """
+    name = dest.__tablename__ if is_model(dest) else dest.name
+    conn = engine.raw_connection()
+    cursor = conn.cursor()
+    formatted_flags = '({})'.format(format_flags(flags)) if flags else ''
+    copy = 'COPY {} FROM STDOUT {}'.format(name, formatted_flags)
+    cursor.copy_expert(copy, source)
+    conn.commit()
     conn.close()
 
 def format_flags(flags):
@@ -76,5 +100,5 @@ def is_model(class_):
     try:
         class_mapper(class_)
         return True
-    except UnmappedClassError:
+    except SQLAlchemyError:
         return False
--- tests/test_export.py
+++ tests/test_export.py
@@ -4,7 +4,7 @@ import pytest
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 
-from postgres_copy import copy_to
+from postgres_copy import copy_to, copy_from
 
 Base = declarative_base()
 engine = sa.create_engine('postgresql:///export-test')
@@ -33,31 +33,67 @@ def objects(session):
     try:
         yield rows
     finally:
-        for row in rows:
-            session.delete(row)
-        session.commit()
+        engine.execute(Album.__table__.delete())
 
-class TestExport:
+class TestCopyTo:
 
-    def test_export_query(self, session, objects):
+    def test_copy_query(self, session, objects):
         sio = io.StringIO()
-        copy_to(session.query(Album), session.connection().engine, sio)
+        copy_to(session.query(Album), sio, session.connection().engine)
         lines = sio.getvalue().strip().split('\n')
         assert len(lines) == 3
         assert lines[0].split('\t') == [str(objects[0].id), objects[0].name]
 
-    def test_export_table(self, session, objects):
+    def test_copy_table(self, session, objects):
         sio = io.StringIO()
-        copy_to(Album.__table__.select(), session.connection().engine, sio)
+        copy_to(Album.__table__.select(), sio, session.connection().engine)
         lines = sio.getvalue().strip().split('\n')
         assert len(lines) == 3
         assert lines[0].split('\t') == [str(objects[0].id), objects[0].name]
 
-    def test_export_csv(self, session, objects):
+    def test_copy_csv(self, session, objects):
         sio = io.StringIO()
         flags = {'format': 'csv', 'header': True}
-        copy_to(session.query(Album), session.connection().engine, sio, **flags)
+        copy_to(session.query(Album), sio, session.connection().engine, **flags)
         lines = sio.getvalue().strip().split('\n')
         assert len(lines) == 4
         assert lines[0].split(',') == ['id', 'name']
         assert lines[1].split(',') == [str(objects[0].id), objects[0].name]
+
+class TestCopyFrom:
+
+    def test_copy_model(self, session, objects):
+        sio = io.StringIO()
+        sio.write('\t'.join(['4', 'The Works']))
+        sio.seek(0)
+        copy_from(sio, Album, session.connection().engine)
+        assert session.query(Album).count() == len(objects) + 1
+        row = session.query(Album).filter_by(id=4).first()
+        assert row.id == 4
+        assert row.name == 'The Works'
+
+    def test_copy_table(self, session, objects):
+        sio = io.StringIO()
+        sio.write('\t'.join(['4', 'The Works']))
+        sio.seek(0)
+        copy_from(sio, Album.__table__, session.connection().engine)
+        assert session.query(Album).count() == len(objects) + 1
+        row = session.query(Album).filter_by(id=4).first()
+        assert row.id == 4
+        assert row.name == 'The Works'
+
+    def test_copy_csv(self, session, objects):
+        sio = io.StringIO()
+        sio.write(
+            '\n'.join([
+                ','.join(['id', 'name']),
+                ','.join(['4', 'The Works'])
+            ])
+        )
+        sio.seek(0)
+        flags = {'format': 'csv', 'header': True}
+        copy_from(sio, Album, session.connection().engine, **flags)
+        assert session.query(Album).count() == len(objects) + 1
+        row = session.query(Album).filter_by(id=4).first()
+        assert row.id == 4
+        assert row.name == 'The Works'
